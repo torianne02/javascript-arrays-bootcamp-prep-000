f760fbad6d64d9491995f5193ae4f99cdeb3371b
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-03-13 18:31:58 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,23 +1,17 @@
-// var ingredient1 = "bread"
-// var ingredient2 = "mild cheese"
-// var ingredient3 = "sharp cheese"
-// var ingredient4 = "butter"
-// var ingredient5 = "tomato"
-// var ingredient6 = "garlic"
-
-// var grilledCheeseIngredients = [
-//   'bread',
-//   'mild cheese',
-//   'sharp cheese',
-//   'butter',
-//   'tomato',
-//   'garlic'
-// ]
- 
-// var tomatoSauceIngredients = [
-//   'tomato',
-//   'garlic',
-//   'olive oil',
-//   'basil',
-//   'oregano'
-// ]
+var chocolateBars = [
+  "snickers", 
+  "hundred grand", 
+  "kitkat", 
+  "skittles"
+  ];
+  
+  function addElementToBeginningOfArray(array, element) {
+    array.push(element);
+    return array;
+  }
+  
+  function destructiveAddElementToBeginningOfArray(array, element) {
+    var newArray = array;
+    newArray.push(element);
+    return newArray;
+  }
